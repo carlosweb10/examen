77ac420e42ee68e8ac34a5db670a2396e6fa267b
carlosweb10 <llanqueteresa190@gmail.com>
2023-11-16 07:54:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Instituto Honorio Delgado
+        </p>
+        <p>
+          Carlos Andres Calvo Ticona
+        </p>
+        <p>
+          Turno Noche
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
